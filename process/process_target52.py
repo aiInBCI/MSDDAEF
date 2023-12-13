2ab8aae89ac63f98294f8bebcd46b0666463ecc4
HZUBCI <153710799+HZUBCI@users.noreply.github.com>
2023-12-13 15:28:00 +0800

    Update process_target52.py

--- process/process_target52.py
+++ process/process_target52.py
@@ -6,11 +6,10 @@ import h5py
 from scipy.signal import butter, lfilter
 from scipy import signal
 
-# 迁移实验，目标域52人数据集处理。
-# src = './data52'
-src = r'D:\苗团队\苗老师\杨忠\韩国52人数据集\52data'
-# out = './target_20_'
-out = '../波幅调整参数计算/target_adjust'
+# data alignment for GIST
+src = './GIST/
+out = './cd_GIST'
+
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -23,21 +22,14 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = signal.filtfilt(b,a,data,axis=2)
     return y
 
-
-
-
-
 def read_data(num):
-    # onset:左手（右手）想象运动开始的点
-    # ele：选取的20导联
+    # onset of MI
     onset = [1024, 4608	,8192	,11776,	15360,	18944	,22528	,26112	,29696	,33280	,36864	,40448,	44032	,47616	,51200	,54784	,58368	,61952	,65536	,69120	,72704	,76288	,79872	,83456	,87040	,90624	,94208	,97792	,101376	,104960	,108544	,112128	,115712	,119296	,122880	,126464	,130048	,133632	,137216	,140800	,144384	,147968	,151552	,155136	,158720	,162304	,165888	,169472	,173056	,176640	,180224	,183808	,187392	,190976	,194560	,198144	,201728	,205312	,208896	,212480	,216064	,219648	,223232	,226816	,230400	,233984	,237568	,241152	,244736	,248320	,251904	,255488	,259072	,262656	,266240	,269824	,273408	,276992	,280576	,284160	,287744	,291328	,294912	,298496	,302080	,305664	,309248	,312832	,316416	,320000	,323584	,327168	,330752	,334336,	337920	,341504	,345088	,348672	,352256	,355840]
-    ele = [9, 10, 11, 46, 45, 44, 14, 13, 12, 48, 49, 50, 51, 17, 18, 19, 32, 56, 55, 54]  #20个导联的索引
-
-
+    ele = [9, 10, 11, 46, 45, 44, 14, 13, 12, 48, 49, 50, 51, 17, 18, 19, 32, 56, 55, 54]  # index of 20 channels
     filename = 's{:02d}.mat'.format(num)
     filepath = pjoin(src, filename)
     raw = loadmat(filepath)
-# 左手，右手想象运动的前50次实验（用作训练集）
+# the first 50 left/right hand MI trials for training
     left = np.zeros([50, 64, 1536])
     right = np.zeros([50, 64, 1536])
     for i in range(50):
@@ -50,17 +42,17 @@ def read_data(num):
         for j in range(1536):
             left_[i, :, j] = left[i, :, j] - np.mean(left[i, :, j])
             right_[i, :, j] = right[i, :, j] - np.mean(right[i, :, j])
-# 选取20导联
+# select 20 channels
     _left = np.zeros([50, 20, 1536])
     _right = np.zeros([50, 20, 1536])
     for i in range(50):
         for j in range(20):
             _left[i][j] = left_[i][ele[j]-1, :]
             _right[i][j] = right_[i][ele[j] - 1, :]
-# 8-30带通滤波
+# bandpass filter 
     buffer_left = butter_bandpass_filter(_left, 8, 30, 512)
     buffer_right = butter_bandpass_filter(_right, 8, 30, 512)
-# 选取0.5-2.5秒时间段，以50为一个单位向后滑动四次，扩充样本数
+# Select a period of 0.5-2.5 seconds and slide backward four times in a step of 50 to expand the number of samples
     left__ = np.zeros([50, 4, 20, 1024])
     right__ = np.zeros([50, 4, 20, 1024])
     for i in range(50):
@@ -73,7 +65,7 @@ def read_data(num):
     right__ = right__.reshape((200, 20, 1024))
 
     train = np.concatenate((left__, right__), axis=0)      #400*20*1024
-# 左手，右手想象运动后50次实验（用作测试集）
+# the last 50 left/right hand MI trials for evaluation
     left_test = np.zeros([50, 64, 1536])
     right_test = np.zeros([50, 64, 1536])
     for i in range(50):
@@ -86,20 +78,20 @@ def read_data(num):
         for j in range(1536):
             left__test[i, :, j] = left_test[i, :, j] - np.mean(left_test[i, :, j])
             right__test[i, :, j] = right_test[i, :, j] - np.mean(right_test[i, :, j])
-# 选取20导联
+# select 20 channels
     test_left = np.zeros([50, 20, 1536])
     test_right = np.zeros([50, 20, 1536])
     for i in range(50):
         for j in range(20):
             test_left[i][j] = left__test[i][ele[j] - 1, :]
             test_right[i][j] = right__test[i][ele[j] - 1, :]
-# 带通滤波
+# bandpass filter
     buffer_left_test = butter_bandpass_filter(test_left, 8, 30, 512)
     buffer_right_test = butter_bandpass_filter(test_right, 8, 30, 512)
 
     test__left = np.zeros([50, 20, 1024])
     test__right = np.zeros([50, 20, 1024])
-# 选取0.5-2.5秒时间段
+# Select a period of 0.5-2.5 seconds
     for i in range(50):
         for j in range(20):
             test__left[i][j] = buffer_left_test[i][j][256:1280]
@@ -107,10 +99,10 @@ def read_data(num):
 
     test = np.concatenate((test__left, test__right), axis=0)    #100*20*1024
 
-    X = np.concatenate((train, test), axis=0)     ##400*20*1024+100*20*1024
-# 调整波形大小
+    X = np.concatenate((train, test), axis=0)     
+# Amplitude adjustment
     X = X/33.674670216208554
-# 对应label值，‘1’左手，‘0’右手。
+# label，‘1’：left hand，‘0’：right hand
     Y_L_T = np.ones(200)
     Y_R_T = np.zeros(200)
     Y_train = np.concatenate((Y_L_T,Y_R_T), axis=0)   #400
@@ -119,19 +111,16 @@ def read_data(num):
     Y_R_test = np.zeros(50)
     Y_test = np.concatenate((Y_L_test,Y_R_test), axis=0)  #100
 
-    Y = np.concatenate((Y_train,Y_test),axis=0)     #400+100
+    Y = np.concatenate((Y_train,Y_test),axis=0)     
 
     return X, Y
 
 
-
-# 读取52人数据处理并保存
 with h5py.File(pjoin(out, 'ku_mi_smt.h5'), 'w') as f:
     for num in tqdm(range(1, 53)):
         X, Y = read_data(num)
         X = X.astype(np.float32)
         Y = Y.astype(np.int64)
-        f.create_dataset('s' + str(num) + '/X', data=X)   ##400*20*1024+100*20*1024
-        f.create_dataset('s' + str(num) + '/Y', data=Y)   #400+100
+        f.create_dataset('s' + str(num) + '/X', data=X)   
+        f.create_dataset('s' + str(num) + '/Y', data=Y)   
 
-###Checked 2022.12.25 mmm
