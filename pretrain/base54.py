14a0aa8c83619eaf7e4b1262e71ec67f3d0cf23a
HZUBCI <153710799+HZUBCI@users.noreply.github.com>
2023-12-13 15:47:40 +0800

    Update base54.py

--- pretrain/base54.py
+++ pretrain/base54.py
@@ -13,15 +13,14 @@ import matplotlib.pyplot as plt
 import time
 
 '''
-针对目标域是54人数据集的预训练模型
+pretraining when openBMI is the target domain 
 '''
 subjs = [35, 47, 46, 37, 13, 27, 12, 32, 4, 40, 19, 41, 18, 42, 34, 7,
          49, 9, 5, 48, 29, 15, 21, 17, 31, 45, 1, 38, 51, 8, 11, 16, 28, 44, 24,
          52, 3, 26, 39, 50, 6, 23, 2, 14, 25, 20, 10, 33, 22, 43, 36, 30]
 
-#dfile = h5py.File('./source/KU_mi_smt.h5', 'r')
-dfile = h5py.File('./adjust_52/ku_mi_smt.h5', 'r')     #20个导联的数据，Data aligment之后的数据
-outpath = './pretrain_model_54/'
+dfile = h5py.File('../process/cd_GIST/ku_mi_smt.h5', 'r')     # data path of GIST after alignment
+outpath = './pretrain_model_54/'  # data path of pretrained models
 
 def get_data(subj):
     dpath = 's' + str(subj) + '/'
@@ -30,7 +29,7 @@ def get_data(subj):
     return np.array(X), np.array(Y)
 
 
-def get_multi_data(subjs):   #读取54人的数据，整合。  54*400
+def get_multi_data(subjs):   
     Xs = []
     Ys = []
     for s in subjs:
@@ -42,10 +41,10 @@ def get_multi_data(subjs):   #读取54人的数据，整合。  54*400
     return X, Y
 
 
-def evaluate(model, x, y):    #进行模型评估
+def evaluate(model, x, y):   
     # print(x.shape, y.shape)
     data_set = TensorDataset(x, y)
-    data_loader = DataLoader(dataset=data_set, batch_size=64, shuffle=True)   #batch size 为64
+    data_loader = DataLoader(dataset=data_set, batch_size=64, shuffle=True)   
     model.eval()
     num = 0
     with torch.no_grad():
@@ -64,10 +63,10 @@ print(cv_set.shape)
 kf = KFold(n_splits=6)
 device = torch.device('cuda')
 
-patience = 20  # 当验证集损失在连续20次训练周期中没有降低，停止训练模型
+patience = 20  # When the validation set loss does not decrease in 20 consecutive training epoches, the training is stopped
 
-cv_loss = np.ones([6])  # 交叉验证 最小验证损失
-result = pd.DataFrame(columns=('cv_index', 'test_acc', 'loss'))  # 数据框架通过append字段填充 即res
+cv_loss = np.ones([6])  # Minimum validation loss for saving each fold
+result = pd.DataFrame(columns=('cv_index', 'test_acc', 'loss'))  # Training process record
 train_loss2 = np.zeros([6,100])
 test_loss2 = np.zeros([6,100])
 train_accuracy2 = np.zeros([6,100])
@@ -75,7 +74,7 @@ test_accuracy2 = np.zeros([6,100])
 
 start_time = time.time()
 
-for cv_index, (train_index, test_index) in enumerate(kf.split(cv_set)):  # cv_index交叉验证次数
+for cv_index, (train_index, test_index) in enumerate(kf.split(cv_set)):  
     tra_acc = np.zeros([100])
     tra_loss = np.zeros([100])
     tst_acc = np.zeros([100])
@@ -106,17 +105,17 @@ for cv_index, (train_index, test_index) in enumerate(kf.split(cv_set)):  # cv_in
     train_set = TensorDataset(X_train, Y_train)
     test_set = TensorDataset(X_test, Y_test)
 
-    train_loader = DataLoader(dataset=train_set, batch_size=32, shuffle=True)    #batch size为32
+    train_loader = DataLoader(dataset=train_set, batch_size=32, shuffle=True)  
     test_loader = DataLoader(dataset=test_set, batch_size=32, shuffle=True)
 
     model = deep().to(device)
     # 优化器adamW改为SGD
     optimizer = torch.optim.AdamW(model.parameters(), lr=1 * 0.0001, weight_decay=0.5 * 0.001)
 
-    train_losses = []  # 每一个epoch的训练损失列表，新的epoch会重空开始
-    test_losses = []  # 每一个epoch的训练损失列表，新的epoch会重空开始
-    train_loss_avg = []  # 训练损失求平均 针对batch求平均
-    test_loss_avg = []  # 训练损失求平均
+    train_losses = []  # A list of training losses for each epoch
+    test_losses = []  #  A list of evaluation losses for each epoch
+    train_loss_avg = []  # Averaging training losses by batch
+    test_loss_avg = []  # veraging evaluation losses by batch
     early_stop = EarlyStopping(patience, delta=0.0001, path=pjoin(outpath, 'model_cv{}.pt'.format(cv_index)),
                                verbose=True)
 
@@ -132,7 +131,7 @@ for cv_index, (train_index, test_index) in enumerate(kf.split(cv_set)):  # cv_in
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if i % 50 == 0:   #每隔50个batch打印一下
+            if i % 50 == 0:   
                 print('\n Train, epoch: {}, i: {}, loss: {}'.format(epoch, i, loss))
 
         e = 0
@@ -155,44 +154,29 @@ for cv_index, (train_index, test_index) in enumerate(kf.split(cv_set)):  # cv_in
         tst_acc[epoch] = e / 4500
 
         if test_loss < cv_loss[cv_index]:
-            cv_loss[cv_index] = test_loss  # 不断迭代的得到每一次交叉验证的最小验证损失
+            cv_loss[cv_index] = test_loss  # Iterate to get the minimum validation loss for each fold
 
-        train_losses = []
-        test_losses = []  # 每一个epoch的验证损失列表，新的epoch会重空开始
+        train_losses = [] # update
+        test_losses = []  # update
 
         test_acc = evaluate(model, X_test, Y_test)
         print('\n Test: acc: {}'.format(test_acc))
 
         res = {'cv_index': cv_index, 'test_acc': test_acc.item(), 'loss': test_loss}
-        result = result.append(res, ignore_index=True)  # pd创造的数据格式
+        result = result.append(res, ignore_index=True) 
 
         early_stop(test_loss, model)
         if early_stop.early_stop:
             print('Early stopping')
-            break  # 判断是否达到早停条件并保存模型
+            break  # Determine whether the early stop condition is met and save the model
     train_loss2[cv_index] = tra_loss
     test_loss2[cv_index] = tst_loss
     train_accuracy2[cv_index] = tra_acc
     test_accuracy2[cv_index] = tst_acc
 
-    # plt.subplot(2, 2, 1)
-    # plt.plot(tra_acc)
-    # plt.subplot(2, 2, 2)
-    # plt.plot(tst_acc)
-    # plt.subplot(2, 2, 3)
-    # plt.plot(tra_loss)
-    # plt.subplot(2, 2, 4)
-    # plt.plot(tst_loss)
-    # plt.show()
-#np.save('train_loss2',train_loss2)
-#np.save('test_loss2',test_loss2)
-#np.save('train_accuracy2',train_accuracy2)
-#np.save('test_accuracy2',test_accuracy2)
-
+   
 end_time = time.time()
 print('cost %f second' % (end_time -  start_time))
 
-min_index = np.argmin(cv_loss, axis=0)  # 六折交叉验证最小验证损失序号
+min_index = np.argmin(cv_loss, axis=0)  # index of six-fold cross validation minimum validation loss 
 result.to_csv(pjoin(outpath, 'result_cv{}.csv'.format(min_index)), index=False)
-
-##Checked 2022.12.26 mmm
